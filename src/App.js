3c0cc893ac1de48272afd16a81b4116c4b383007
Medea Pruidze <medeapruidze3@gmail.com>
2021-08-24 18:38:04 +0400

    basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
+import Values from 'values.js';
 
 function App() {
-  return (
-    <section>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('gellow');
+  }
+  return <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          name='color'
+          value={color}
+          placeholder='#f15025'
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button className='btn' type='submit' >generate</button>
+      </form>
     </section>
-  )
+    <section className="colors">
+      <h4>list goes here</h4>
+    </section>
+  </>
 }
 
 export default App;
